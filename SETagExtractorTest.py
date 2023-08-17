3a76262b38c938ec0e5a22cdba2d53390a0f57db
dksysd <dksysd@kumoh.ac.kr>
2023-08-17 20:47:46 +0900

    오타 수정

--- SETagExtractorTest.py
+++ SETagExtractorTest.py
@@ -1,3 +1,4 @@
 from SETagExtractor import SETagExtractor
 
-SETagExtractor(user='sig', password='sig1234', host='119.63.246.52', db='SIG').tag_all()
+SETagExtractor(user='sig', password='sig1234', host='119.63.246.52', db='SIG').tag_all(reset_tag_post_table=True,
+                                                                                       reset_keyword_post_table=True)
